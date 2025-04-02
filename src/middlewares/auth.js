4ebc73031e6acad67d75e501ed12ec589b49a8d0
Pau <misscotters@gmail.com>
2025-04-02 19:07:16 +0200

    fixed login payload and token structure for auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,10 @@
 import { sendResponseAccessDenied } from '../utils/responses.js';
 import { checkPermission, getDataFromToken, generateAccessToken} from '../services/authService.js';
 import { getRolesHasPermissionsModel } from '../models/authorization/roles_has_permissionsModel.js';
-import mysql from '../adapters/mysql.js';
 import { error404, errorHandler } from '../utils/errors.js';
 import { noResults } from '../validators/result-validators.js';
+import mysql from '../adapters/mysql.js';
+import { sendResponseNotFound } from '../utils/responses.js';
 
 const obtainToken = (req, res) => {
     return new Promise((resolve, reject) => {
@@ -11,18 +12,15 @@ const obtainToken = (req, res) => {
         if (token) {
             resolve(token);
         } else {
-            sendResponseAccessDenied(res, { message: 'No authorization provided. Access token required' });
-            reject(new Error('No authorization provided'));
+            return sendResponseAccessDenied(res, { message: 'No authorization provided. Access token required' })
         }
     });
 };
 
 const setToken = (result, req, res, next, config) => {
-	const { user, role } = result._data
-	const token = generateAccessToken({
-		payload: { user, role },
-		config
-	})
+	const { uuid, role } = result._data
+    const payload = {role, user: uuid}
+	const token = generateAccessToken(payload)
 	next({ user: { ...result, token } })
 }
 
@@ -34,8 +32,7 @@ const authenticateToken = (req, res, next) => {
             next();
         })
         .catch((error) => {
-            console.error('Error in authentication middleware:', error);
-            sendResponseAccessDenied(res, { message: 'Access denied. Invalid token.' });
+            return sendResponseAccessDenied(res, error);
         });
 };
 
@@ -44,31 +41,27 @@ const authorizePermission = (endpoint) => {
         obtainToken(req, res)
             .then((token) => getDataFromToken(token)) //extract user data from the token
             .then((decoded) => {
-                const roleName = decoded.payload.role
+                const roleName = decoded.role
                 _getRolePermissionsByName(roleName, config)
                     .then((rolePermissions) => {
                         const action = req.method
                         //check if the user has the necessary permissions
-                        return checkPermission(action, endpoint, rolePermissions)
+                        return checkPermission(action, endpoint, rolePermissions, config)
                             .then(({ hasPermission }) => {
                                 if (!hasPermission) {
-                                    sendResponseAccessDenied(res, {
-                                        message: `You don't have permission to ${action} on ${endpoint}`
+                                    return sendResponseAccessDenied(res, {
+                                        message: `Access denied. User does not have permission`
                                     });
-                                    throw new Error(`Permission denied for ${action} on ${endpoint}`);
                                 }
-                            
-                                //attach user and role to the request object
-                                //req.auth.user = user;
-                                //req.auth.role = role;
+                                req.auth.user = decoded.user
+                                req.auth.role = roleName;
                             
                                 next(); 
                             })
                         })
             })
             .catch((error) => {
-                console.error('Error in authorization middleware:', error);
-                sendResponseAccessDenied(res, { message: 'Authorization error', error: error.message });
+                return sendResponseAccessDenied(res, error);
             });
     };
 };
@@ -82,15 +75,13 @@ const _getRolePermissionsByName = (roleName, config) => {
                 const error = errorHandler(err, config.environment)
                 return sendResponseNotFound(res, error)
             }
-            console.log(response)
             return response.map(({ permission_action, permission_endpoint }) => ({
                 permission_action,
                 permission_endpoint
             }))
         })
         .catch((err) => {
-            const error = errorHandler(err, config.environment)
-            res.status(error.code).json(error)
+            errorHandler(err, config.environment)
         })
         .finally(() => {
             mysql.end(conn)
