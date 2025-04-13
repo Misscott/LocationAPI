b0a76c000435cf26098443d4dc59e2a4dc097d96
Pau <misscotters@gmail.com>
2025-04-14 01:17:26 +0200

    wip db

--- src/config/development.dist.js
+++ src/config/development.dist.js
@@ -1,11 +1,11 @@
 export default {
 	environment: 'development',
-	port: 3000,
+	port: 3306,
 	bodyLimit: '300kb',
 	db: {
-		host: 'localhost',
-		user: 'root',
-		password: '1234',
+		host: 'ls-f85b70f391f2c9e6cf335dafef04161a8a719f25.cf88wacmc99x.eu-west-3.rds.amazonaws.com',
+		user: 'dbmasteruser',
+		password: 'NZU~jdR28NWC25qw5o81m|ki#Ib?a{Nk',
 		timezone: '+00:00',
 		decimalNumbers: true
 	}
